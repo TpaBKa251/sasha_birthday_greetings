96e4066a5781d1404ca9039e7a0f854e1c58c200
Лапшин Илья Романович <irl1@tpu.ru>
2024-08-19 11:06:50 +0700

    Готово

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './App.css'; // Импортируем стили
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [greetings, setGreetings] = useState([]);
+    const baseUrl = 'http://localhost:8080/sasha/birthday/social/network'; // базовый URL для статических файлов
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const sections = document.querySelectorAll('.greeting-card');
+
+            sections.forEach(section => {
+                const rect = section.getBoundingClientRect();
+                if (rect.top <= window.innerHeight * 0.9 && rect.bottom >= 0) {
+                    section.classList.add('visible');
+                }
+            });
+        };
+
+        const fetchGreetings = async () => {
+            const response = await fetch('http://localhost:8080/sasha/birthday/social/network/get');
+            const data = await response.json();
+            setGreetings(data);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        fetchGreetings();
+
+        // Убедитесь, что классы применяются при первой загрузке страницы
+        handleScroll();
+
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+    return (
+        <div className="app-container">
+            <h1>С днем рождения!</h1>
+            <div className="greetings-container">
+                {greetings.map((greeting) => (
+                    <div className="greeting-card" key={greeting.id}>
+                        <h2 className="greeting-name">{greeting.name}</h2>
+                        <p className="greeting-message">{greeting.message}</p>
+                        <div className="media-container">
+                            {greeting.imageUrl && (
+                                <img src={`${baseUrl}${greeting.imageUrl}`} alt="Greeting Image" className="greeting-image" />
+                            )}
+                            {greeting.videoUrl && (
+                                <video src={`${baseUrl}${greeting.videoUrl}`} controls className="greeting-video" />
+                            )}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
